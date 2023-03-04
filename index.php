92cdc4cc59e88bec180b107c8704743a14201b08
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-04 15:03:17 +0300

    main

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ if(isset($update['message']))
         $user->update($tg_user);
         $telega->sendMes(MY_ID, "Пишет <b>$user->first_name $user->last_name</b> \nДата старта: $user->date");
         require_once __DIR__ . "/functions/work.php";
+        $telega->sendMes($chat_id, $msg['text']);
         if (hasHello($msg['text']) == true)
         {
             $hi = goodTime();
