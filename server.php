0e2ebde489c1de109bae0e373d46faf43cbf7e9b
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-04 22:07:28 +0300

    main

--- server.php
+++ server.php
@@ -2,15 +2,14 @@
  require_once __DIR__ . '/autoload.php';
  $base = new BaseAPI;
  
- $row = 1;
- if (($handle = fopen("/data/products.csv", "r")) !== FALSE) {
-     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-         $num = count($data);
-         echo "<p> $num полей в строке $row: <br /></p>\n";
-         $row++;
-         for ($c=0; $c < $num; $c++) {
-             echo $data[$c] . "<br />\n";
-         }
+
+ if (($handle = fopen(__DIR__ . "/data/products.csv", "r")) !== FALSE) {
+     while (($data = fgetcsv($handle, 2000, ",")) !== FALSE) {
+         
+         $base->addProduct($data[0],$data[1]);
+         echo $data[0]." - ".$data[1]. "<br/>";
+         
      }
      fclose($handle);
+     
  }
