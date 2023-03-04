704083edb58734292b59b3882e0364ffbfe65b3a
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-04 10:11:21 +0300

    main

--- server.php
+++ server.php
@@ -4,10 +4,10 @@
  
     $hiMes = 'Здравствуйте';
  
-      if ($time > 4 && $time < 10) {  $hiMes = 'Доброе утро'}
-      if ($time > 9 && $time < 18) {  $hiMes = 'Добрый день'}
-      if ($time >17 && $time < 24) {  $hiMes = 'Добрый вечер'}
-      if ($time >-1 && $time < 5 ) {  $hiMes = 'Доброй ночи'}
+      if ($time > 4 && $time < 10) {  $hiMes = 'Доброе утро';}
+      if ($time > 9 && $time < 18) {  $hiMes = 'Добрый день';}
+      if ($time >17 && $time < 24) {  $hiMes = 'Добрый вечер';}
+      if ($time >-1 && $time < 5 ) {  $hiMes = 'Доброй ночи';}
       
 echo "HEllo WORLD!!!!!! - " . $time +3 . "<br>";
 echo $hiMes;
