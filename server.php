f1d65ab6c65314bfe723bb84ced58e84060f75b2
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-04 10:10:28 +0300

    main

--- server.php
+++ server.php
@@ -3,13 +3,11 @@
     $time = date("H"); // 17:16:18
  
     $hiMes = 'Здравствуйте';
-    //time = parseInt(time);
-    //time = 2;
-    //Logger.log(time);
-  /*
-      if ($time > 4 && time < 10) {  hiMes = 'Доброе утро'}
-      if (time > 9 && time < 18) {  hiMes = 'Добрый день'}
-      if (time >17 && time < 24) {  hiMes = 'Добрый вечер'}
-      if (time >-1 && time < 5 ) {  hiMes = 'Доброй ночи'}
-      */
-echo "HEllo WORLD!!!!!! - " . $time +3;
+ 
+      if ($time > 4 && $time < 10) {  $hiMes = 'Доброе утро'}
+      if ($time > 9 && $time < 18) {  $hiMes = 'Добрый день'}
+      if ($time >17 && $time < 24) {  $hiMes = 'Добрый вечер'}
+      if ($time >-1 && $time < 5 ) {  $hiMes = 'Доброй ночи'}
+      
+echo "HEllo WORLD!!!!!! - " . $time +3 . "<br>";
+echo $hiMes;
