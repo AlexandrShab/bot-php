dcc78b7c07a8a3b10b37d2a6275721251d6f00a5
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-04 11:28:11 +0300

    main

--- functions/work.php
+++ functions/work.php
@@ -11,21 +11,26 @@ function isMesFind(sample)
     else return false;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function goodTime(){
-  var date = new Date();
-  //var time = Utilities.formatDate(date, 'GMT+3', 'yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'');//date.getHours()
-  var time = Utilities.formatDate(date, 'GMT+3', 'HH');//получается строковая переменная
-  let hiMes = 'Здравствуйте';
-  time = parseInt(time);
-  //time = 2;
-  //Logger.log(time);
+function goodTime()
+{
+    date_default_timezone_set('Europe/Moscow'); 
+    //$today = date("Y-m-d");
+    $time = date("H"); // 17:16:18
+    //$today = date("Y-m-d H:i:s"); // 2001-03-10 17:16:18 (the MySQL DATETIME format)
+    $hiMes = 'Здравствуйте';
+ 
+      if ($time > 5 && $time < 10) {  $hiMes = 'Доброе утро';}
+      if ($time > 9 && $time < 18) {  $hiMes = 'Добрый день';}
+      if ($time >17 && $time < 24) {  $hiMes = 'Добрый вечер';}
+      if ($time >-1 && $time < 6 ) {  $hiMes = 'Доброй ночи';}
 
-    if (time > 4 && time < 10) {  hiMes = 'Доброе утро'}
-    if (time > 9 && time < 18) {  hiMes = 'Добрый день'}
-    if (time >17 && time < 24) {  hiMes = 'Добрый вечер'}
-    if (time >-1 && time < 5 ) {  hiMes = 'Доброй ночи'}
-    
-    //Logger.log(hiMes)
-    return hiMes
+    return $hiMes;
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function writeToExpertKeyboard()
+{
+    var keyboard = array(
+        'inline_keyboard' => [
+      ['text' => '▶️ НАПИСАТЬ ЭКСПЕРТУ', 'url' => 'http://t.me/blondin_man'],
+                        ]);
+}
