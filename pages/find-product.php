0e487f1de90687901694e37ff63f403bdc7acc01
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-10 00:36:47 +0300

    main

--- pages/find-product.php
+++ pages/find-product.php
@@ -18,21 +18,22 @@
     </div>
     <script>
         var webApp = window.Telegram.WebApp;
-        var style = webApp.themeParams;     webApp.ready();
+        var style = webApp.themeParams;     
+        
         document.getElementById('list').innerHTML = JSON.stringify(themeParams);
               
         document.body.style.backgroundColor = style.bg_color;
         document.body.style.color = style.text_color;
         document.button.style.backgroundColor = style.button_color;
         document.button.style.color = style.button_text_color;
-   
-         const styles ={"bg_color":"#ffffff",
+   webApp.ready();
+       /*  const styles ={"bg_color":"#ffffff",
                 "button_color":"#40a7e3",
                 "button_text_color":"#ffffff",
                 "hint_color":"#999999",
                 "link_color":"#168acd",
                 "secondary_bg_color":"#f1f1f1",
-                "text_color":"#000000"};
+                "text_color":"#000000"};*/
     </script>
 </body>
 </html>
