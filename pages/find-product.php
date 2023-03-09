c45a14830f8d43d6a7cffddc3c15eab281c94a5e
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-10 00:18:03 +0300

    main

--- pages/find-product.php
+++ pages/find-product.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
     <title>SertSale</title>
 </head>
 <body>
@@ -12,5 +13,15 @@
         <input type="text" name="query" placeholder="Введите название товара">
         <button type="submit">Проверить</button>
     </form>
+    <div id="list">
+
+    </div>
+    <script>
+        var webApp = window.Telegram.WebApp;
+        var themeParams = webApp.themeParams;
+        window.getElementById('list').innerHTML = JSON.stringify(themeParams);
+              webApp.ready();
+            
+    </script>
 </body>
 </html>
