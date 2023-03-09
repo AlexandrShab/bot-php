583f97db65a25953592f29a2ffc30e21fbfbff12
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-10 00:50:47 +0300

    main

--- serv.php
+++ serv.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+    <title>SertSale</title>
+</head>
+<body>
 <?php
 
 require_once __DIR__ . '/autoload.php';
@@ -179,3 +189,27 @@ function csvToBase($base)
         fclose($handle);
     }
 }
+?>
+ <script>
+        var webApp = window.Telegram.WebApp;
+        var style = webApp.themeParams;     
+        
+        //document.getElementById('list').innerHTML = JSON.stringify(style);
+              
+        document.body.style.backgroundColor = style.bg_color;
+        document.body.style.color = style.text_color;
+        document.querySelectorAll('button').style.backgroundColor = style.button_color;
+       // document.button.style.backgroundColor = style.button_color;
+       document.querySelectorAll('button').style.backgroundColor = style.button_text_color;
+        //document.button.style.color = style.button_text_color;
+   webApp.ready();
+       /*  const styles ={"bg_color":"#ffffff",
+                "button_color":"#40a7e3",
+                "button_text_color":"#ffffff",
+                "hint_color":"#999999",
+                "link_color":"#168acd",
+                "secondary_bg_color":"#f1f1f1",
+                "text_color":"#000000"};*/
+    </script>
+</body>
+</html>
