f414cd7ebb52c38d08481be92944837845f26f50
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-10 00:30:43 +0300

    main

--- pages/find-product.php
+++ pages/find-product.php
@@ -20,7 +20,12 @@
         var webApp = window.Telegram.WebApp;
         var style = webApp.themeParams;
         //document.getElementById('list').innerHTML = JSON.stringify(themeParams);
-              webApp.ready();
+              
+        document.body.style.backgroundColor = style.bg_color;
+        document.body.style.color = style.text_color;
+        document.button.style.backgroundColor = style.button_color;
+        document.button.style.color = style.button_text_color;
+        webApp.ready();
          const styles ={"bg_color":"#ffffff",
                 "button_color":"#40a7e3",
                 "button_text_color":"#ffffff",
