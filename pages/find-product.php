ca4b5a5dded24bbeb2954650cd7b32e2ed1f3c4c
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-10 23:43:30 +0300

    main

--- pages/find-product.php
+++ pages/find-product.php
@@ -30,7 +30,7 @@
             text-align: center;
             margin: 0 auto;
             width: 100%;
-            height: 120px;
+            height: 100px;
             color: white;
             background: black;
             min-width: 330px;
