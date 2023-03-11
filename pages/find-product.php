60f85e6b646fe7dff13673c79e790649809ced75
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-11 21:57:06 +0300

    main

--- pages/find-product.php
+++ pages/find-product.php
@@ -64,7 +64,7 @@
         webApp.MainButton.show();
         //webApp.MainButton.onClick(document.location.href='https://t.me/blrAlex');
         //webApp.showConfirm('Подтверждение'); 
-/*            
+           
         //document.getElementById('list').innerHTML = JSON.stringify(data);
         //document.getElementById('list').innerHTML = JSON.stringify(style);
             
@@ -72,20 +72,19 @@
          document.querySelectorAll('#content').style.color = style.text_color;
         document.querySelectorAll('button').style.backgroundColor = style.button_color;
        // document.button.style.backgroundColor = style.button_color;
-       document.querySelectorAll('button').style.backgroundColor = style.button_text_color;
+       document.querySelectorAll('button').style.color = style.button_text_color;
         //document.button.style.color = style.button_text_color;
-        webApp.BackButton.show();
-        webApp.MainButton.show();
+       
         
         webApp.ready();   
-  */
+  
        /*  const styles ={"bg_color":"#ffffff",
                 "button_color":"#40a7e3",
                 "button_text_color":"#ffffff",
                 "hint_color":"#999999",
                 "link_color":"#168acd",
                 "secondary_bg_color":"#f1f1f1",
-                "text_color":"#000000"};*/
+                "text_color":"#000000"};
             const appData =    {"query_id":"AAEauLg5AAAAABq4uDmPIyie",
                             "user":{"id":968407066,
                                 "first_name":"Alex",
@@ -94,7 +93,7 @@
                                 "language_code":"ru",
                                 "is_premium":true},
                             "auth_date":"1678476891",
-                            "hash":"f4dd11e37760871ba8773979860514647e427080b559d83cc029488139e81c08"}
+                            "hash":"f4dd11e37760871ba8773979860514647e427080b559d83cc029488139e81c08"};*/
     </script>
 </body>
 </html>
