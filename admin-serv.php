51547bc7aac9313b4288d003d3fbe2f10b71f197
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-12 15:15:05 +0300

    main

--- admin-serv.php
+++ admin-serv.php
@@ -1,16 +1,66 @@
 <?php
-header('Access-Control-Allow-Origin: *');/*
+header('Access-Control-Allow-Origin: *');
 ?>
-<auth>
-    <script async src="https://telegram.org/js/telegram-widget.js?19\" 
-                        data-telegram-login="ByStatBot" 
-                        data-size="medium" 
-                        data-auth-url="http://sertbot.shinny-mir.by/check_authorization.php" 
-                        data-request-access="write"
-                        style="padding-top: 16px; float: right;"
-                        ></script> 
-</auth>
-<?php*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <title>SertSale</title>
+    <style>
+        .main-title {
+            font-family: "Playfair Display", Roboto, Helvetica, Arial, sans-serif;
+            font-size: 1.2rem;
+            line-height: 1.133;
+            font-weight: 600;
+            letter-spacing: .02em;
+            display: block;
+            text-align: center;
+            margin: 20px auto;
+            width: 90%;
+            max-width: 960px;
+            min-width: 300px;
+            
+        }
+        .brand{
+            font-family: "Playfair Display", Roboto, Helvetica, Arial, sans-serif;
+            font-size: 2.5rem;
+            font-weight: 900;
+            letter-spacing: 0.03em;
+            display: grid;
+            text-align: center;
+            margin: 0 auto;
+            width: 100%;
+            height:100px;
+            color: white;
+            background: black;
+            min-width: 330px;
+            align-items: center;
+        } 
+        #content {
+            height: 200px;
+            border-radius:  30px;
+            /*border: solid black 1px;*/
+        }
+        #btn {
+            justify-self: end;
+            height: 40px;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>
+    
+</head>
+<body>
+    <div class="brand" style="backgroundColor: black;">
+        <text>SertSale_bot</text>
+    </div>
+    <div id="content">
+        <p1 class="main-title">Страница администратора</p1>
+<?php
 //require_once __DIR__ . '/classes/request.php';
 
 require_once __DIR__ . '/autoload.php';
@@ -115,4 +165,9 @@ function getTelegramUserData() {
     }
     return false;
   }
-  
+  ?>
+     </div><!-- конец класса content-->
+
+    
+</body>
+</html>
