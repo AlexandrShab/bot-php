d398d40f260c804d43726ac50ba0144216b9183c
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-12 18:42:39 +0300

    main

--- pages/admin-page.php
+++ pages/admin-page.php
@@ -49,15 +49,8 @@
             border-radius: 5px;
         }
     </style>
-    <auth>
-            <script async src="https://telegram.org/js/telegram-widget.js?19\" 
-                        data-telegram-login="ByStatBot" 
-                        data-size="medium" 
-                        data-auth-url="http://topbots.site/check_authorization.php" 
-                        data-request-access="write"
-                        style="padding-top: 16px; float: right;"
-                        ></script>
-                    </auth>
+   
+           
 </head>
 <body>
     <div class="brand" style="backgroundColor: black;">
