8fb1a3e87774c55f9a314c1d265dc2f4a15f5fc0
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-12 14:41:54 +0300

    main

--- admin-serv.php
+++ admin-serv.php
@@ -34,23 +34,24 @@ if ($tg_user->isAdmin == '1'){$isAdmin = true;}
 if ($tg_user !== false) {
     //var_dump($tg_user);
   $first_name = htmlspecialchars($tg_user->first_name);
-  
+  //$html .= "<name style=\"float:right;\">{$first_name}</name>";
   
     $html .= "<a href=\"/admin-serv.php?logout=1\" 
         class=\"dropdown\" style=\"float:right;\">
         <img src=\"/public/img/door1.jpg\"
             style=\"height:40px;margin-right:10px;\">
         </a>";
-    $html .= "<name style=\"float:right;\">{$first_name}</name>";
+    
  if (isset($tg_user->photo_url)) {
     $photo_url = htmlspecialchars($tg_user->photo_url);
     //$photo_url = $tg_user['photo_url'];
     $html .= "<img src=\"{$photo_url}\" style=\"width:40px; 
             border-radius:20px;float:right;margin-right:10px;\">";
+    
   }else
       {
           $html .= "<name style=\"float:right;margin:10px;
-                color:white;\">{$first_name}</name>";
+                color:black;\">{$first_name}</name>";
       }
    print_r($html); 
 }else {
