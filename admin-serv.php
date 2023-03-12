eff3ba256210739879a6c7c06c016dab71ad2259
AlexandrShab <93769197+AlexandrShab@users.noreply.github.com>
2023-03-12 13:24:45 +0300

    main

--- admin-serv.php
+++ admin-serv.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin: *');
     <script async src="https://telegram.org/js/telegram-widget.js?19\" 
                         data-telegram-login="ByStatBot" 
                         data-size="medium" 
-                        data-auth-url="http://topbots.site/check_authorization.php" 
+                        data-auth-url="http://sertbot.shinny-mir.by/check_authorization.php" 
                         data-request-access="write"
                         style="padding-top: 16px; float: right;"
                         ></script> 
